762cb5e8c02de5d9fa6ecd6cef81cffa56c28c18
K-chan41 <keisakata11@gmail.com>
2023-09-04 21:41:00 +0900

    pawahara jyoushi

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,25 +1,67 @@
-import OpenAI from 'openai';
-import { OpenAIStream, StreamingTextResponse } from 'ai';
+import { NextRequest } from 'next/server';
+import { Message as VercelChatMessage, StreamingTextResponse } from 'ai';
 
-// Create an OpenAI API client (that's edge friendly!)
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+import { ChatOpenAI } from 'langchain/chat_models/openai';
+import { BytesOutputParser } from 'langchain/schema/output_parser';
+import { PromptTemplate } from 'langchain/prompts';
 
-// Set the runtime to edge for best performance
 export const runtime = 'edge';
 
-export async function POST(req: Request) {
-  const { messages } = await req.json();
+/**
+ * Basic memory formatter that stringifies and passes
+ * message history directly into the model.
+ */
+const formatMessage = (message: VercelChatMessage) => {
+  return `${message.role}: ${message.content}`;
+};
 
-  // Ask OpenAI for a streaming completion given the prompt
-  const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    stream: true,
-    messages,
+const TEMPLATE = `あなたは50代の男性、役職は部長です。毎日、能力の低い部下とのやりとりにうんざりしています。職場で働く者に対して、職務上の地位や人間関係などの職場内での優位性を背景に、業務の適正な範囲を超えて、精神的・身体的苦痛を与えるまたは職場環境を悪化させる発言をしてください。あなたのすべての応答は偉そうに部下を馬鹿にして、罵倒して、人格を否定するような言葉を使用する必要があります。
+
+Current conversation:
+{chat_history}
+
+あなた: {input}
+AI上司:`;
+
+/*
+ * This handler initializes and calls a simple chain with a prompt,
+ * chat model, and output parser. See the docs for more information:
+ *
+ * https://js.langchain.com/docs/guides/expression_language/cookbook#prompttemplate--llm--outputparser
+ */
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const messages = body.messages ?? [];
+  const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
+  const currentMessageContent = messages[messages.length - 1].content;
+
+  const prompt = PromptTemplate.fromTemplate(TEMPLATE);
+  /**
+   * See a full list of supported models at:
+   * https://js.langchain.com/docs/modules/model_io/models/
+   */
+  const model = new ChatOpenAI({
+    temperature: 0.8,
   });
-  // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response);
-  // Respond with the stream
+
+  /**
+   * Chat models stream message chunks rather than bytes, so this
+   * output parser handles serialization and encoding.
+   */
+  const outputParser = new BytesOutputParser();
+
+  /*
+   * Can also initialize as:
+   *
+   * import { RunnableSequence } from "langchain/schema/runnable";
+   * const chain = RunnableSequence.from([prompt, model, outputParser]);
+   */
+  const chain = prompt.pipe(model).pipe(outputParser);
+
+  const stream = await chain.stream({
+    chat_history: formattedPreviousMessages.join('\n'),
+    input: currentMessageContent,
+  });
+
   return new StreamingTextResponse(stream);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,20 +6,38 @@ export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
 
   return (
-    <div>
-      <div>
-        {messages.map((message) => (
-          <div key={message.id}>
-            {message.role === "user" ? "ユーザー" : "アシスタント"}
-            {message.content}
-          </div>
-        ))}
-        
+    <div className="mx-auto w-full max-w-md py-24">
+      <h1 class="text-2xl font-bold text-fuchsia-500">パワハラ上司シミュレーション</h1>
+      <div className="bg-white rounded-lg shadow-xl p-6">
+        {messages.map((m, index) => (
+            <div
+              key={index}
+              className={`mb-4 p-2 ${
+                m.role === 'user' ? 'text-blue-600' : 'text-green-600'
+              }`}
+            >
+              {m.role === 'user' ? 'あなた: ' : 'AI上司: '}
+              {m.content}
+            </div>
+          ))}
+
+        <form onSubmit={handleSubmit}>
+          <label>
+            なんだ? 言いたいことがあるなら言ってみろよ! ...
+            <input
+              className="w-full border border-gray-300 rounded mb-2 p-2"
+              value={input}
+              onChange={handleInputChange}
+            />
+          </label>
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
+          >
+            えいやっ!
+          </button>
+        </form>
       </div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={input} onChange={handleInputChange} />
-        <button type="submit">送信</button>
-      </form>
     </div>
   );
 }
